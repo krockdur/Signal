aa5b7b24895a42ee3d506c6fe5805d057edabd40
Julien Fougery <web@fougery.ovh>
2023-04-22 20:58:22 +0200

    .

--- src/signal/main/defines.lua
+++ src/signal/main/defines.lua
@@ -7,8 +7,11 @@ print("coucou")
 local config = {}
 
 config.ENEMY_WIDTH = 40
-config.TIME_BETWEEN_POP = 1.2
+config.TIME_BETWEEN_ENEMY_POP = 1.2
+config.TIME_BETWEEN_ENEMY_POP_HARD = 3.5
 
+config.TIME_LOSE_PER_HURT = 3
+config.TIME_LOSE_PER_EARTH_HURT = 8
 
 config.ENEMY_SPEED = 50
 
@@ -17,6 +20,6 @@ config.TYPE_ENEMY_GREEN = 2
 config.TYPE_ENEMY_WHITE = 3
 config.TYPE_ENEMY_RED = 4
 
-config.MAX_PLAYER_TIME = 5
+config.MAX_PLAYER_TIME = 30
 
 return config
